ce1543aec271575901f180d4c621b579e99c5279
Srijan Das <dassrijan16@gmail.com>
2024-08-11 11:13:10 +0530

    merge BST, largest BST in binary tree

--- BST/BST.java
+++ BST/BST.java
@@ -140,9 +140,101 @@ public class BST {
 
         return root;
     }
+
+    //largest BST in Binary Tree
+    static int MAX_SIZE = Integer.MIN_VALUE;
+    static class NodeInfo{
+        boolean isBST;
+        int size;
+        int max;
+        int min;
+
+        public NodeInfo(boolean b, int s, int max, int min){
+            this.isBST = b;
+            this.size = s;
+            this.max = max;
+            this.min = min;
+        }
+    }
+
+    public static NodeInfo findLargest(Node root){
+        if(root == null)
+            return new NodeInfo(true, 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        
+        NodeInfo left = findLargest(root.left);
+        NodeInfo right = findLargest(root.right);
+        int size = left.size + right.size + 1;
+        int min = Math.min(Math.min(left.min, right.min), root.val);
+        int max = Math.max(Math.max(left.max, right.max), root.val);
+
+        if(root.val <= left.max|| root.val >= right.min){
+            return new NodeInfo(false, size, max, min);
+        }
+        if(left.isBST && right.isBST){
+            MAX_SIZE = Math.max(MAX_SIZE, size);
+            return new NodeInfo(true, size, max, min);
+        }
+        return new NodeInfo(false, size, max, min);
+    }
+
+    //merging 2 BSTs
+    public static void inorderList(Node root, ArrayList<Integer> list){
+        if(root == null)
+            return;
+        inorderList(root.left, list);
+        list.add(root.val);
+        inorderList(root.right, list);
+    }
+    public static ArrayList<Integer> mergeList(ArrayList<Integer> l1, ArrayList<Integer> l2){
+        ArrayList<Integer> l = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while( i < l1.size() && j < l2.size()){
+            if(l1.get(i) < l2.get(j)){
+                l.add(l1.get(i++));
+            }else if(l1.get(i) > l2.get(j)){
+                l.add(l2.get(j++));
+            }else{
+                l.add(l1.get(i));
+                i++;
+                j++;
+            }
+        }
+        while(i < l1.size()){
+            l.add(l1.get(i++));
+        }
+        while(j < l2.size()){
+            l.add(l2.get(j++));
+        }
+
+        return l;
+    }
+
+    //overloading
+    static Node createBalBST(ArrayList<Integer> list, int start, int end){
+        if(start > end)
+            return null;
+
+        int mid = start + (end - start)/2;
+        Node root = new Node(list.get(mid));
+        root.left = createBalBST(list, start, mid-1);
+        root.right = createBalBST(list, mid+1, end);
+
+        return root;
+    }
+
+    public static Node mergeBSTs(Node root1, Node root2){
+        ArrayList<Integer> l1 = new ArrayList<>();
+        ArrayList<Integer> l2 = new ArrayList<>();
+        inorderList(root1, l1);
+        inorderList(root2, l2);
+        ArrayList<Integer> l = mergeList(l1, l2);
+        Node root = createBalBST(l, 0, l.size()-1);
+        return root;   
+    }
     public static void main(String[] args) {
         //8,5,3,1,4,6,10,11,14
-        int [] values = {1,1,1};
+        int [] values = {8,5,3,1,4,6,10,11,14};
         Node root = null;
         for(int i = 0; i < values.length; i++){
             root = insert(root, values[i]);
@@ -160,6 +252,11 @@ public class BST {
         //creating a balanced BST
         int nodes[] = {3,5,6,8,10,11,12};
         Node balBSTRoot = createBalBST(nodes, 0, nodes.length-1);
-        inOrder(balBSTRoot);
+        // inOrder(balBSTRoot);
+        findLargest(balBSTRoot);
+        System.out.println();
+        System.out.println("Largest BST in the given BT size: "+ MAX_SIZE);
+        System.out.println();
+        inOrder(mergeBSTs(root, balBSTRoot));
     }
 }
